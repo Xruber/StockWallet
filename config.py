7c2227dfe6150eeea6dd6aed429184aa1fd7f924
Xruber <106792429+Xruber@users.noreply.github.com>
2026-02-11 09:31:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@
 # ==========================================
 
 # 1. Your NEW Bot Token from BotFather
-BOT_TOKEN = "YOUR_NEW_WALLET_BOT_TOKEN_HERE"
+BOT_TOKEN = "8573917866:AAEZaREXTEsiTVwCB3R14YISJMHDg9GBsL4"
 
 # 2. Your MongoDB URI (Paste it right here)
 MONGO_URI = "mongodb+srv://YOUR_USERNAME:YOUR_PASSWORD@your-cluster.mongodb.net/..."
 
 # 3. Your Numeric Telegram ID (so you get deposit/withdraw requests)
-ADMIN_ID = 123456789 
+ADMIN_ID = 6239774927 
 
 # 4. Link to your UPI QR Code image
-PAYMENT_IMAGE_URL = "https://example.com/your_qr_code.jpg"
+PAYMENT_IMAGE_URL = "https://cdn.discordapp.com/attachments/888361275464220733/1451949298928455831/Screenshot_20251029-1135273.png?ex=698d3f68&is=698bede8&hm=d59390c159648c0df2b5b1f9fba5420ee3a7223c95dc47e22d1babf4933cebf0&"
